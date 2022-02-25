7b782ddd3dfff108693cb23306372d35b14e90c1
stewanoya <stewartanoya@gmail.com>
2022-02-25 11:24:09 -0500

    create 2 threads to count at the same time

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Multithreading_practice
 {
@@ -6,7 +7,48 @@ namespace Multithreading_practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // A thread is the execution path of a program
+            // - We can use multiple threads to perform different tasks at the same time
+            // - Current thread running is "Main" thread
+            // - requires using System.Threading
+
+            Thread mainThread = Thread.CurrentThread;
+            mainThread.Name = "Main Thread";
+            Console.WriteLine(mainThread.Name);
+
+            // if method doesn't have params, you can call threads to run methods like this
+            /* Thread thread1 = new Thread(CountDown);
+            Thread thread2 = new Thread(CountUp); */
+
+            // if method has params, use lambda function (aka anonymous function)
+            Thread thread1 = new Thread(() => CountDown("Timer 1"));
+            Thread thread2 = new Thread(() => CountUp("Timer 2"));
+
+            thread1.Start();
+            thread2.Start();
+
+
+            Console.WriteLine(mainThread.Name + " is complete!");
+            Console.ReadKey();
+        }
+
+        public static void CountDown(string name)
+        {
+            for (int i = 10; i >= 0; i--)
+            {
+                Console.WriteLine( name + ": " + i + " seconds");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Timer #1 Complete!");
+        }
+        public static void CountUp(string name)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(name + ": " + i + " seconds");
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Timer #2 Complete!");
         }
     }
 }
